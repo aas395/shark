48d167d3d543665bff9a90608e3f9be9a4788272
Ryan Luu <ryanluu@Ryans-MacBook-Pro.local>
2017-06-24 23:29:23 -0400

    Added Start Button

--- game-system.js
+++ game-system.js
@@ -18,7 +18,7 @@ AFRAME.registerSystem('game', {
     },
     forwardMotionCoefficient: {
       type : "number",
-      default : 2
+      default : 0
     },
     currentLevel: {
       type : "number",
@@ -38,7 +38,7 @@ AFRAME.registerSystem('game', {
   },
   tick: function() {
     if(this.data.hasStarted) {
-      this.updateScore();  
+      this.updateScore();
     }
   },
   setDistance: function(distance) {
@@ -54,5 +54,19 @@ AFRAME.registerSystem('game', {
   setLevelEnd: function(end) {
     this.data.levelEnd = end;
     // console.log(this.data.levelEnd)
+  },
+  setHasStarted: function(hasStarted) {
+    this.data.hasStarted = hasStarted;
+  },
+  startGame: function() {
+    this.data.forwardMotionCoefficient = 2;
+    this.data.score = 0;
+    this.data.time = 0;
+    this.data.hasStarted = true;
+    this.data.currentLevel = 1;
+
+    if(document.querySelector(modal)) {
+        //hide modal
+    }
   }
-});    
+});
