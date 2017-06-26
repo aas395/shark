629c04847e0e2de722558479f2395cf2b4dfac67
Aaron Smyth <aas395@gmail.com>
2017-06-25 22:40:53 -0400

    fixing gaze animation

--- button.js
+++ button.js
@@ -9,25 +9,26 @@ AFRAME.registerComponent('cursor-listener', {
 		var that = this;
 
 		this.el.addEventListener('mouseenter', function (evt) {
-			var animation = document.createElement("a-animation");
-			animation.setAttribute("begin", "cursor-fusing");
-			animation.setAttribute("easing", "ease-in");
-			animation.setAttribute("attribute", "scale");
-			animation.setAttribute("fill", "forwards");
-			animation.setAttribute("from", "0.1 0.1 0.1");
-			animation.setAttribute("to", "0.05 0.05 0.05");
-			animation.setAttribute("dur", 1200);
-			animation.setAttribute("id", "cursor-animation");
-			that.data.target.appendChild(animation);
+			document.querySelector('#status-bar-progress').emit('button-mouseover');
+			// var animation = document.createElement("a-animation");
+			// animation.setAttribute("begin", "cursor-fusing");
+			// animation.setAttribute("easing", "ease-in");
+			// animation.setAttribute("attribute", "scale");
+			// animation.setAttribute("fill", "forwards");
+			// animation.setAttribute("from", "0.1 0.1 0.1");
+			// animation.setAttribute("to", "0.05 0.05 0.05");
+			// animation.setAttribute("dur", 1000);
+			// animation.setAttribute("id", "cursor-animation");
+			// that.data.target.appendChild(animation);
 		});
 
 		this.el.addEventListener('mouseleave', function (evt) {
-			var cursorAnimation = document.querySelector('#cursor-animation');
-			document.querySelector('#cursor').removeChild(cursorAnimation);
+			document.querySelector('#status-bar-progress').emit('button-mouseout');
+			// var cursorAnimation = document.querySelector('#cursor-animation');
+			// document.querySelector('#cursor').removeChild(cursorAnimation);
 		});
 
 		this.el.addEventListener('click', function (evt) {
-			console.log('click');
 			Game.startGame();
 		});
 	}
