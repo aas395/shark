693dd5bb02d124a99333b3fbf77acc91ff92f7c5
Aaron Smyth <aas395@gmail.com>
2017-06-29 21:45:13 -0400

    introducing faster x/y movement

--- player.js
+++ player.js
@@ -1,5 +1,37 @@
 AFRAME.registerComponent("player", {
-  init: function() {},
+  init: function() {
+    var self = this;
+    this.XYmultiplier = 1.1;
+
+    this.el.addEventListener('loaded', function() {
+
+    });
+
+    this.el.addEventListener('collide', function(e) {
+
+      if(e.detail.body.el.hasAttribute('tunnel')) {
+        var Game = document.querySelector('a-scene').systems['game'];
+        var player = self.el;
+
+        var playerPosition = player.getAttribute('position');
+        var xPosition = playerPosition.x;
+        var yPosition = playerPosition.y;
+        var zPosition = playerPosition.z;
+
+        var xDirectionAdjustment = xPosition > 0 ? -self.XYmultiplier : self.XYmultiplier;
+        var yDirectionAdjustment = yPosition > 0 ? -self.XYmultiplier : self.XYmultiplier;
+
+        if(Game.data.hasStarted) {
+          // console.log('test');
+          self.el.setAttribute('position', {
+            x: xPosition + (5 * xDirectionAdjustment),
+            y: yPosition + (5 * yDirectionAdjustment),
+            z: zPosition
+          });  
+        }
+      }
+    });
+  },
   tick : function() {
       var Game = document.querySelector('a-scene').systems['game'];
 
@@ -8,8 +40,8 @@ AFRAME.registerComponent("player", {
         const direction = this.el.components.camera.camera.getWorldDirection();
 
         var currentPosition = {
-          x: playerPosition.x + direction.x,
-          y: playerPosition.y + direction.y,
+          x: playerPosition.x + (direction.x * this.XYmultiplier),
+          y: playerPosition.y + (direction.y * this.XYmultiplier),
           z: playerPosition.z
         };
 
